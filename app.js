2b657d7862a08baf0e29aa3d01a010dc6c06231b
Greg Tarsa <gltarsa@gmail.com>
2017-01-02 12:57:04 -0500

    Adds routing and second controller

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.controller('mainController', function() {
-    
+myApp.config(function($routeProvider, $locationProvider) {
+
+  $locationProvider.hashPrefix('');
+  $routeProvider
+    .when('/', {
+      templateUrl: 'pages/main_page.html',
+      controller: 'mainController'
+    })
+
+  $routeProvider
+    .when('/second', {
+      templateUrl: 'pages/second_page.html',
+      controller: 'mainController'  //using the same controller for now
+    })
+
 });
+
+myApp.controller('mainController', [ '$scope', '$filter', function($scope, $filter) {
+
+  $scope.handle = ""
+
+  // update the lc and uc handle versions whenever handle changes.
+  $scope.$watch('handle', function(newVal, oldVal) {
+    console.log('newVal: ' + newVal);
+    $scope.lcHandle = $filter('lowercase')(newVal);
+    $scope.ucHandle = $filter('uppercase')(newVal);
+  });
+
+}]);
